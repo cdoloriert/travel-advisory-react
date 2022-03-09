f4a0f110e0c8957726590ed86c9f345bc5c8e07d
CD <cdoloriert@gmail.com>
2022-03-09 08:27:33 -0500

    latest changes

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles.js';
+import { mergeClasses } from '@material-ui/styles';
+
+
+const Map = () => {
+
+    const coords = { lat: 0, lng: 0 };
+
+    return (
+        <div className={mergeClasses.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBjSAccY8-NDHhIsaJHTsNguNZ-bP9AgJA' }}
+                defaultCenter={coords}
+                center={coords}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            // onChange={(e) => {
+            //     setCoords({ lat: e.center.lat, lng: e.center.lng });
+            //     setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+            // }}
+            // onChildClick={(child) => setChildClicked(child)}
+            >
+
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+export default Map;
